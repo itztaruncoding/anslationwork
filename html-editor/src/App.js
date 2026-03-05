9602cab790e87f4cb092d59e87a8bf6e901588c3
Tarun Pandagre <tarunpandagre108@gmail.com>
2026-03-05 17:18:22 +0530

    Update App.js

--- html-editor/src/App.js
+++ html-editor/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+
 import Home from "./components/Home";
 import Features from "./components/Features";
 import FAQ from "./components/FAQ";
